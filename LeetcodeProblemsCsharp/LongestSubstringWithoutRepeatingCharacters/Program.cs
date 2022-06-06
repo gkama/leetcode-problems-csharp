a92efe3ee56170dadcca11973a21ffa61c8e258a
Georgi Kamacharov <gkamacharov@gmail.com>
2022-06-06 15:10:11 -0400

    add longest substring

---
+++ LeetcodeProblemsCsharp/LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine(Solution.LengthOfLongestSubstring("abcabcbb"));
+Console.WriteLine(Solution.LengthOfLongestSubstring("bbbbb"));
+Console.WriteLine(Solution.LengthOfLongestSubstring("pwwkew"));
+Console.WriteLine(Solution.LengthOfLongestSubstring("abcdefghijklm"));
+Console.WriteLine(Solution.LengthOfLongestSubstring("a"));
+Console.WriteLine(Solution.LengthOfLongestSubstring("aa"));
---
+++ LeetcodeProblemsCsharp/LongestSubstringWithoutRepeatingCharacters/Solution.cs
@@ -0,0 +1,49 @@
+﻿// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+public static class Solution
+{
+    public static int LengthOfLongestSubstring(string s)
+    {
+        if (string.IsNullOrEmpty(s)) return 0;
+
+        var streaks = new List<int>();
+        var streak = false;
+        var total = 0;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            var nonRepeatedChars = new HashSet<char>();
+            nonRepeatedChars.Add(s[i]);
+            total += 1;
+
+            for (var j = i + 1; j < s.Length; j++)
+            {
+                if (!nonRepeatedChars.Contains(s[j]))
+                {
+                    nonRepeatedChars.Add(s[j]);
+                    streak = true;
+                }
+                else
+                {
+                    streak = false;
+                    streaks.Add(total);
+                    total = 0;
+                    break;
+                }
+
+                if (streak)
+                {
+                    total += 1;
+                }
+
+                if (streak && j == s.Length - 1) //Reached the end
+                {
+                    streak = false;
+                    streaks.Add(total);
+                    total = 0;
+                }
+            }
+        }
+
+        return streaks.Count == 0 ? 1 : streaks.Max();
+    }
+}
