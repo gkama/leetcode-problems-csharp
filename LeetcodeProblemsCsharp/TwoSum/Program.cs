e1e5642787a580aab4c9664b9e73ec6275a09330
Georgi Kamacharov <gkamacharov@gmail.com>
2022-06-06 13:47:55 -0400

    update to cleanup

--- LeetcodeProblemsCsharp/TwoSum/Program.cs
+++ LeetcodeProblemsCsharp/TwoSum/Program.cs
@@ -1,3 +1,7 @@
 ﻿var res1 = string.Join(", ", Solution.TwoSum(new int[] { 2, 7, 11, 15 }, 9));
+var res2 = string.Join(", ", Solution.TwoSum(new int[] { 3, 2, 4 }, 6));
+var res3 = string.Join(", ", Solution.TwoSum(new int[] { 3, 3 }, 6));
 
 Console.WriteLine($"[{res1}]");
+Console.WriteLine($"[{res2}]");
+Console.WriteLine($"[{res3}]");
