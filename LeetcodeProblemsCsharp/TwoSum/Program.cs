c69da5bc007d4c40b5fb1c52c23c07bb4bb89125
Georgi Kamacharov <gkamacharov@gmail.com>
2022-06-06 13:43:21 -0400

    add twosum O(n^2)

---
+++ LeetcodeProblemsCsharp/TwoSum/Program.cs
@@ -0,0 +1,3 @@
+﻿var res1 = string.Join(", ", Solution.TwoSum(new int[] { 2, 7, 11, 15 }, 9));
+
+Console.WriteLine($"[{res1}]");
---
+++ LeetcodeProblemsCsharp/TwoSum/Solution.cs
@@ -0,0 +1,28 @@
+﻿public static class Solution
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        return BigONSquared(nums, target);
+    }
+
+    // O(n^2)
+    private static int[] BigONSquared(int[] nums, int target)
+    {
+        var indices = new List<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    indices.AddRange(new int[] { i, j });
+
+                    goto found;
+                }
+            }
+        }
+
+        found: return indices.ToArray();
+    }
+}
