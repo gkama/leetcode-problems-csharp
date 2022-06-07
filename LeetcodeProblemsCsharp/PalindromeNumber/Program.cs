54408b2499717769da84bb4662b1dc2b88e7b021
Georgi Kamacharov <gkamacharov@gmail.com>
2022-06-07 09:30:20 -0400

    update palindrome

---
+++ LeetcodeProblemsCsharp/PalindromeNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿var exs = new int[]
+{
+    121,
+    123,
+    -121,
+    10,
+    101,
+    -1,
+    8,
+    1001
+};
+
+foreach (var ex in exs)
+{
+    Console.WriteLine($"{ex} = {Solution.IsPalindrome(ex)}");
+}
---
+++ LeetcodeProblemsCsharp/PalindromeNumber/Solution.cs
@@ -0,0 +1,34 @@
+﻿//https://leetcode.com/problems/palindrome-number/
+public static class Solution
+{
+    public static bool IsPalindrome(int x)
+    {
+        var xStr = x.ToString();
+
+        if (x == 0 || xStr.Count() == 1)
+        {
+            return true;
+        }
+        else if (xStr.Count() == 2 || xStr.Count() == 3)
+        {
+            return xStr[0] == xStr[xStr.Length - 1];
+        }
+
+        var isEven = xStr.Length % 2 == 0;
+        var half = xStr.Length / 2;
+        var left = xStr.Substring(0, half);
+
+        if (isEven)
+        {
+            var right = xStr.Substring(half, xStr.Length - half);
+
+            return left == new string(right.Reverse().ToArray());
+        }
+        else
+        {
+            var right = xStr.Substring(half + 1, xStr.Length - (half + 1));
+
+            return left == new string(right.Reverse().ToArray());
+        }
+    }
+}
