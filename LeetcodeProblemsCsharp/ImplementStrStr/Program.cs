52eaacea3bb8ecd96f5a76facc1e1999c7764197
Georgi Kamacharov <gkamacharov@gmail.com>
2022-06-08 11:20:29 -0400

    add implement str str

---
+++ LeetcodeProblemsCsharp/ImplementStrStr/Program.cs
@@ -0,0 +1,15 @@
+﻿var exs = new Dictionary<string, string>
+{
+    { "hello", "ll" },
+    { "aaaaa", "bba" },
+    { "abcabc", "abc" },
+    { "aaabb", "bb" }
+};
+
+
+foreach (var ex in exs)
+{
+    var res = Solution.StrStr(ex.Key, ex.Value);
+
+    Console.WriteLine($"{ex.Value} in {ex.Key} -> {res}");
+}
---
+++ LeetcodeProblemsCsharp/ImplementStrStr/Solution.cs
@@ -0,0 +1,40 @@
+﻿// https://leetcode.com/problems/implement-strstr/
+public class Solution
+{
+    public static int StrStr(string haystack, string needle)
+    {
+        /*
+         * return index of the first occurrence of needle in haystack
+         * or -1 if needle is not part of haystack
+         * 0 when needle is an empty string
+         * 
+         * Alg
+         *      hello -> ll
+         *      hello -> op
+         */
+        if (string.IsNullOrEmpty(needle)) return 0;
+        else if (string.IsNullOrEmpty(haystack)) return -1;
+        // What if they're both null or empty? Does that mean that needle is in the haystack
+
+        var needleFirstLetter = needle[0];
+        var needleLength = needle.Length;
+        var haystackLength = haystack.Length;
+
+        for (var i = 0; i < haystack.Length; i++)
+        {
+            if (haystack[i] == needleFirstLetter)
+            {
+                if (needleLength > haystackLength - i)
+                { 
+                    return -1;
+                }
+                else if (needle == haystack.Substring(i, needleLength))
+                {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
