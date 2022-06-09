6536cabd34e3825b7ef4b084de5bd7747eed80ec
Georgi Kamacharov <gkamacharov@gmail.com>
2022-06-09 12:45:48 -0400

    add length of last word

---
+++ LeetcodeProblemsCsharp/LengthOfLastWord/Program.cs
@@ -0,0 +1,15 @@
+﻿var exs = new List<string>
+{
+    "a ",
+    "Hello World",
+    "   fly me   to   the moon  ",
+    "luffy is still joyboy",
+    "     ",
+    " ",
+    "                 fly me         "
+};
+
+foreach (var ex in exs)
+{
+    Console.WriteLine($"{ex} -> {Solution.LengthOfLastWord2(ex)}");
+}
---
+++ LeetcodeProblemsCsharp/LengthOfLastWord/Solution.cs
@@ -0,0 +1,27 @@
+﻿// https://leetcode.com/problems/length-of-last-word/
+public class Solution
+{
+    /*
+     * Alg
+     *      trim end string
+     *      split into array based on " "
+     *      return last one's length
+     */
+    public static int LengthOfLastWord(string s)
+    {
+        var trimmed = s.Trim();
+        var split = trimmed.Split(' ');
+        var splitLast = split[split.Length - 1];
+        var len = splitLast.Length;
+
+        return len;
+    }
+
+    public static int LengthOfLastWord2(string s)
+    {
+        var split = s.Split(' ').ToHashSet();
+        var len = split.Last(x => !string.IsNullOrWhiteSpace(x)).Length;
+
+        return len;
+    }
+}
