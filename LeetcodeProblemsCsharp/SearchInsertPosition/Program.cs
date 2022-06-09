1d1c914bed8b5198b88cdbf33ebc1d2a2c8734c8
Georgi Kamacharov <gkamacharov@gmail.com>
2022-06-09 12:32:51 -0400

    add search insert position

---
+++ LeetcodeProblemsCsharp/SearchInsertPosition/Program.cs
@@ -0,0 +1,14 @@
+﻿var exs = new Dictionary<int[], int>
+{
+    { new int[] { 1, 3, 5, 6 }, 5 },
+    { new int[] { 1, 3, 5, 6 }, 2 },
+    { new int[] { 1, 3, 5, 6 }, 7 },
+    { new int[] { 1, 2, 3, 4, 5, 6 }, 7 },
+    { new int[] { 1, 3, 5, 6 }, 0 },
+    { new int[] { 1, 3 }, 2 }
+};
+
+foreach (var ex in exs)
+{
+    Console.WriteLine($"{ex.Value} into {string.Join(",", ex.Key)} -> {Solution.SearchInsert(ex.Key, ex.Value)}");
+}
---
+++ LeetcodeProblemsCsharp/SearchInsertPosition/Solution.cs
@@ -0,0 +1,42 @@
+﻿// https://leetcode.com/problems/search-insert-position/
+public class Solution
+{
+    /*
+     * Alg
+     *      sort of a greedy alg
+     *      divide the array 4 times (binary search)
+     *      this will work great for 
+     *      do a binary search and show where it should be inserted
+     *      this will be O(log n)
+     */
+    public static int SearchInsert(int[] nums, int target)
+    {
+        if (nums == null || nums.Length == 0)
+        {
+            return -1;
+        }
+
+        int i = 0,
+            j = nums.Length - 1;
+
+        while (i <= j)
+        {
+            int m = j + (i - j) / 2;
+
+            if (nums[m] == target)
+            {
+                return m;
+            }
+            else if (nums[m] < target)
+            {
+                i = m + 1;
+            }
+            else
+            {
+                j = m - 1;
+            }
+        }
+
+        return i;
+    }
+}
