96a5ac2ba9f1a8aeddacb50b344e9aaf032d8c80
Georgi Kamacharov <gkamacharov@gmail.com>
2022-06-13 10:55:37 -0400

    add egg drop

---
+++ LeetcodeProblemsCsharp/EggDropWithTwoEggsAndNFloors/Program.cs
@@ -0,0 +1,10 @@
+﻿var exs = new List<int>
+{
+    2,
+    100
+};
+
+foreach (var ex in exs)
+{
+    Console.WriteLine($"{ex} -> {Solution.TwoEggDrop(ex)}");
+}
---
+++ LeetcodeProblemsCsharp/EggDropWithTwoEggsAndNFloors/Solution.cs
@@ -0,0 +1,40 @@
+﻿// https://leetcode.com/problems/egg-drop-with-2-eggs-and-n-floors/
+public static class Solution
+{
+    /*
+     * Alg
+     *      can we do a binary search on it?
+     *      if n = 100
+     *      step 1. drop at floor 50
+     *      step 2. if it breaks then < 50
+     *              if it doesn't break then > 50
+     *      step 3. case one, drop at 75 (100 - 50 / 2)
+     *      step 4. if it breaks 
+     *      ...
+     *      when to exit?
+     */
+    public static int TwoEggDrop(int n)
+    {
+        if (n <= 2)
+        {
+            return n;
+        }
+
+        var totalDrops = 1;
+
+        int TwoEggDropInternal(int n)
+        {
+            if (n - totalDrops <= 0)
+            {
+                return totalDrops;
+            }
+
+            n -= totalDrops;
+            totalDrops++;
+
+            return TwoEggDropInternal(n);
+        }
+
+        return TwoEggDropInternal(n);
+    }
+}
