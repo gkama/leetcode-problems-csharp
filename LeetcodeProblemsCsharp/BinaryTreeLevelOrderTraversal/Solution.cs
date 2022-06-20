c60afc48fef1e7260e9672b4162a9114ea0b8a5c
Georgi Kamacharov <gkamacharov@gmail.com>
2022-06-20 11:19:54 -0400

    add sols

---
+++ LeetcodeProblemsCsharp/BinaryTreeLevelOrderTraversal/Solution.cs
@@ -0,0 +1,46 @@
+﻿// https://leetcode.com/problems/binary-tree-level-order-traversal/
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public static class Solution
+{
+    public static IList<IList<int>> LevelOrder(TreeNode root)
+    {
+        var levelOrderTraversal = new List<IList<int>>();
+
+        if (root == null)
+        {
+            return levelOrderTraversal;
+        }
+
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        while (queue.Count() > 0)
+        {
+            var size = queue.Count();
+            var levelOrder = new List<int>();
+
+            for (var i = 0; i < size; i++)
+            {
+                var next = queue.Dequeue();
+                levelOrder.Add(next.val);
+
+                if (next.left != null) queue.Enqueue(next.left);
+                if (next.right != null) queue.Enqueue(next.right);
+            }
+
+            levelOrderTraversal.Add(levelOrder);
+        }
+
+        return levelOrderTraversal;
+    }
+}
---
+++ LeetcodeProblemsCsharp/InsertIntoABinarySearchTree/Solution.cs
@@ -0,0 +1,52 @@
+﻿// https://leetcode.com/problems/insert-into-a-binary-search-tree/
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public static class Solution
+{
+    /*
+    Alg
+        recursively insert
+        if root is null return new TreeNode(val)
+        root.left = call again with root.left and val
+        root.right = call again with root.right and val
+    */
+    public static TreeNode InsertIntoBST(TreeNode root, int val)
+    {
+        if (root == null)
+        {
+            return new TreeNode(val);
+        }
+
+        return InsertRecursive(root, val);
+    }
+
+    private static TreeNode InsertRecursive(TreeNode root, int val)
+    {
+        if (root == null)
+        {
+            return new TreeNode(val);
+        }
+
+        if (root.val > val)
+        {
+            // Go to the left subtree
+            root.left = InsertRecursive(root.left, val);
+        }
+        else
+        {
+            // Go to the right subtree
+            root.right = InsertRecursive(root.right, val);
+        }
+
+        return root;
+    }
+}
