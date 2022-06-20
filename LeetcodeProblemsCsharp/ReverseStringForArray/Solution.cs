8d5c35a18836fe47ca31905c8c47a8fac6f203a5
Georgi Kamacharov <gkamacharov@gmail.com>
2022-06-20 13:17:57 -0400

    add sols

---
+++ LeetcodeProblemsCsharp/ReverseStringForArray/Solution.cs
@@ -0,0 +1,33 @@
+﻿// https://leetcode.com/problems/reverse-string/
+public static class Solution
+{
+    /*
+    Alg
+        start 0 to middle
+            if length is even, right in the middle
+            if length is odd, middle - 1 (still length / 2 because of int division)
+        swap elements from both sides
+        increment / decrement both sides
+    */
+    public static void ReverseString(char[] s)
+    {
+        if (s == null) return;
+
+        var sLength = s.Length;
+
+        if (sLength == 1) return;
+
+        var mid = sLength % 2 == 0
+            ? sLength / 2
+            : (sLength - 1) / 2;
+        var j = sLength - 1;
+
+        for (var i = 0; i < mid; i++)
+        {
+            var temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            j--;
+        }
+    }
+}
---
+++ LeetcodeProblemsCsharp/RotateArray/Solution.cs
@@ -0,0 +1,58 @@
+﻿// https://leetcode.com/problems/rotate-array/
+public static class Solution
+{
+    /*
+    Alg
+        iterate 0 to k
+        selection sort swapping until the end
+    */
+    public static void Rotate(int[] nums, int k)
+    {
+        if (nums == null || k == 0 || nums.Length == 1)
+        {
+            return;
+        }
+
+        var arr = WithNewArray(nums, k);
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            nums[i] = arr[i];
+        }
+    }
+
+    /*
+    Alg
+        since it doesn't say to not use another array
+        we'll create a new array
+        1. start at nums.length - k and iterate to the end
+            add those elements first to the array
+        2. iterate 0 to nums.length - k 
+            add those elements
+    */
+    private static int[] WithNewArray(int[] nums, int k)
+    {
+        if (nums == null || k == 0)
+        {
+            return null;
+        }
+
+        var numsLength = nums.Length;
+        var rotatedArray = new int[numsLength];
+        var numsLengthMinusK = numsLength - k;
+
+        for (var i = 0; i < numsLength; i++)
+        {
+            if (i < numsLengthMinusK)
+            {
+                rotatedArray[i + k] = nums[i];
+            }
+            else
+            {
+                rotatedArray[(i + k) % numsLength] = nums[i];
+            }
+        }
+
+        return rotatedArray;
+    }
+}
