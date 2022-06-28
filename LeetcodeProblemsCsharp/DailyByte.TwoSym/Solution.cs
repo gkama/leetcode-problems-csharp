f0aa1de33456f3fa0d319d51807301b2d8223d50
Georgi Kamacharov <gkamacharov@gmail.com>
2022-06-28 11:31:58 -0400

    add daily byte two sum

---
+++ LeetcodeProblemsCsharp/DailyByte.TwoSym/Solution.cs
@@ -0,0 +1,40 @@
+﻿public static class Solution
+{
+    public static bool HasTwoSum(int[] nums, int target)
+    {
+        if (nums == null || nums.Length < 2) return false;
+
+        var dictionary = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (dictionary.ContainsKey(target - nums[i]))
+            {
+                return true;
+            }
+            else if (!dictionary.ContainsKey(nums[i]))
+            {
+                dictionary.Add(nums[i], i);
+            }
+        }
+
+        return false;
+    }
+
+    public static void Examples()
+    {
+        var exs = new Dictionary<int, int[]>
+        {
+            { 10, new int[] { 1, 3, 8, 2 } },
+            { 8, new int[] { 3, 9, 13, 7 } },
+            { 4, new int[] { 4, 2, 6, 5, 2 } },
+            { 1, new int[] { -3, 4, 6, 5, 2 } },
+            { -4, new int[] { -3, 4, 6, 5, -1 } },
+        };
+
+        foreach (var ex in exs)
+        {
+            Console.WriteLine($"{ex.Key} in {string.Join(",", ex.Value)} -> {HasTwoSum(ex.Value, ex.Key)}");
+        }
+    }
+}
