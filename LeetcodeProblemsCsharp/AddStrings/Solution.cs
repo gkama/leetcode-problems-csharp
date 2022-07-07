9cacd1ed7d6bf717e0deff60cfec1b5fbb925f0a
Georgi Kamacharov <gkamacharov@gmail.com>
2022-07-07 14:01:22 -0400

    add sols

---
+++ LeetcodeProblemsCsharp/AddStrings/Solution.cs
@@ -0,0 +1,57 @@
+﻿// https://leetcode.com/problems/add-strings/
+public static class Solution
+{
+    public static string AddStrings(string num1, string num2)
+    {
+        if (string.IsNullOrWhiteSpace(num1) && string.IsNullOrWhiteSpace(num2)) return null;
+        if (string.IsNullOrWhiteSpace(num1)) return num2;
+        if (string.IsNullOrWhiteSpace(num2)) return num1;
+
+        // Start from end, add each digits and check for carryover
+        //  start from the smaller number
+        var carryOver = 0;
+
+        var num1CharArray = num1.ToCharArray();
+        var num2CharArray = num2.ToCharArray();
+
+        // Keep 2 indexes
+        var indexNum1 = num1CharArray.Length - 1;
+        var indexNum2 = num2CharArray.Length - 1;
+
+        // Bigger index for creating the array
+        var sumIndex = indexNum1 >= indexNum2 ? indexNum1 : indexNum2;
+
+        // Result
+        var sum = new string[sumIndex + 1];
+
+        // Start at the end and move forward
+        while (indexNum1 >= 0 || indexNum2 >= 0)
+        {
+            var num1Digit = indexNum1 >= 0 ? (int)Char.GetNumericValue(num1CharArray[indexNum1]) : 0;
+            var num2Digit = indexNum2 >= 0 ? (int)Char.GetNumericValue(num2CharArray[indexNum2]) : 0;
+            var sumDigit = num1Digit + num2Digit + carryOver;
+
+            if (sumDigit >= 10)
+            {
+                carryOver = 1;
+                sumDigit = sumDigit - 10;
+            }
+            else
+            {
+                carryOver = 0;
+            }
+
+            sum[sumIndex] = sumDigit.ToString();
+
+            Console.WriteLine($"{sum[sumIndex]}");
+
+            sumIndex--;
+            indexNum1--;
+            indexNum2--;
+        }
+
+        // edge case of carryOver = 1
+
+        return carryOver == 1 ? $"{carryOver}{string.Join("", sum)}" : string.Join("", sum);
+    }
+}
---
+++ LeetcodeProblemsCsharp/PascalsTriangle2/Solution.cs
@@ -0,0 +1,28 @@
+﻿// https://leetcode.com/problems/pascals-triangle-ii/
+public static class Solution
+{
+    public static IList<int> GetRow(int rowIndex)
+    {
+        // Pascal's triangle is a jagged array
+        var pascalsTriangle = new int[rowIndex + 1][];
+
+        // Initialize the inner arrays and one's
+        for (var i = 0; i < rowIndex + 1; i++)
+        {
+            pascalsTriangle[i] = new int[i + 1];
+            pascalsTriangle[i][0] = 1; // First
+            pascalsTriangle[i][i] = 1; // Last
+
+            // Iterate and populate the inner
+            if (i > 1)
+            {
+                for (var j = 1; j < i; j++)
+                {
+                    pascalsTriangle[i][j] = pascalsTriangle[i - 1][j - 1] + pascalsTriangle[i - 1][j];
+                }
+            }
+        }
+
+        return pascalsTriangle[rowIndex];
+    }
+}
