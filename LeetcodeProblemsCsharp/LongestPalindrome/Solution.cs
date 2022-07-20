e4a94ec6a23a1ec75311a0b9db84fb30887e8700
Georgi Kamacharov <gkamacharov@gmail.com>
2022-07-20 11:19:59 -0400

    add sol

---
+++ LeetcodeProblemsCsharp/LongestPalindrome/Solution.cs
@@ -0,0 +1,27 @@
+﻿// https://leetcode.com/problems/longest-palindrome/
+public static class Solution
+{
+    public static int LongestPalindrome(string s)
+    {
+        if (s == null || s.Length == 0)
+            return 0;
+
+        int len = 0;
+        var set = new HashSet<char>();
+
+        foreach (var c in s)
+        {
+            if (set.Contains(c))
+            {
+                len += 2;
+                set.Remove(c);
+            }
+            else
+            {
+                set.Add(c);
+            }
+        }
+
+        return set.Count > 0 ? len + 1 : len;
+    }
+}
