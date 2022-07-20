95efe7b508b822bc91f235d37ab559c9328fdf4b
Georgi Kamacharov <gkamacharov@gmail.com>
2022-07-20 15:45:45 -0400

    add sol

---
+++ LeetcodeProblemsCsharp/WordPattern/Solution.cs
@@ -0,0 +1,34 @@
+﻿// https://leetcode.com/problems/word-pattern/
+public static class Solution
+{
+    public static bool WordPattern(string pattern, string s)
+    {
+        if (pattern == null) return true;
+        if (s == null) return false;
+
+        var sArray = s.Split(' ');
+
+        if (pattern.Length != sArray.Length) return false;
+
+        var dict = new Dictionary<string, string>();
+
+        for (var i = 0; i < sArray.Length; i++)
+        {
+            var word = sArray[i];
+            var patternLetter = pattern[i].ToString();
+
+            if (dict.ContainsKey(patternLetter))
+            {
+                if (dict[patternLetter] != word) return false;
+            }
+            else
+            {
+                if (dict.ContainsValue(word)) return false;
+
+                dict.Add(patternLetter, word);
+            }
+        }
+
+        return true;
+    }
+}
