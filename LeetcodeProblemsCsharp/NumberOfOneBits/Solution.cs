0278c3b160b8625118c1d4a5d615229b84b159d5
Georgi Kamacharov <gkamacharov@gmail.com>
2022-07-22 14:56:46 -0400

    add sol

---
+++ LeetcodeProblemsCsharp/NumberOfOneBits/Solution.cs
@@ -0,0 +1,16 @@
+﻿// https://leetcode.com/problems/number-of-1-bits/
+public static class Solution
+{
+    public static int HammingWeight(uint n)
+    {
+        var nBinary = Convert.ToString(n, 2);
+        var total = 0;
+
+        for (var i = 0; i < nBinary.Length; i++)
+        {
+            if (nBinary[i] == '1') total += 1;
+        }
+
+        return total;
+    }
+}
