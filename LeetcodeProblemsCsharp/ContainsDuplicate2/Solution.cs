6026919cf64e39cc56f0ea546161bd1f741cc63b
Georgi Kamacharov <gkamacharov@gmail.com>
2022-07-25 08:44:16 -0400

    add sols

---
+++ LeetcodeProblemsCsharp/ContainsDuplicate2/Solution.cs
@@ -0,0 +1,62 @@
+﻿// https://leetcode.com/problems/contains-duplicate-ii/
+public class Solution
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        return ContainsNearbyDuplicateV2(nums, k);
+    }
+
+
+    private bool ContainsNearbyDuplicateV2(int[] nums, int k)
+    {
+        if (nums.Length < 1) return false;
+
+        var dict = new Dictionary<int, List<int>>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (dict.ContainsKey(nums[i]))
+            {
+                foreach (var index in dict[nums[i]])
+                {
+                    if (Math.Abs(i - index) <= k) return true;
+                }
+
+                dict[nums[i]].Add(i);
+            }
+            else
+            {
+                dict.Add(nums[i], new List<int> { i });
+            }
+        }
+
+        return false;
+    }
+
+
+    private bool ContainsNearbyDuplicateV1(int[] nums, int k)
+    {
+        if (nums.Length < 1) return false;
+
+        var dict = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (dict.ContainsKey(nums[i]))
+            {
+                if (Math.Abs(i - dict[nums[i]]) <= k)
+                {
+                    return true;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            dict.Add(i, nums[i]);
+        }
+
+        return false;
+    }
+}
---
+++ LeetcodeProblemsCsharp/ImplementStackUsingQueues/Solution.cs
@@ -0,0 +1,120 @@
+﻿// https://leetcode.com/problems/implement-stack-using-queues/
+public class MyStack
+{
+
+    private Queue<int> _queue1;
+    private Queue<int> _queue2;
+
+    public MyStack()
+    {
+        _queue1 = new Queue<int>();
+        _queue2 = new Queue<int>();
+    }
+
+    public void Push(int x)
+    {
+        _queue1.Enqueue(x);
+    }
+
+    public int Pop()
+    {
+        if (_queue1.Count() > 0) return PopQueue1();
+        else if (_queue2.Count() > 0) return PopQueue2();
+
+        return 0;
+    }
+
+    public int Top()
+    {
+        if (_queue1.Count() > 0) return TopQueue1();
+        else if (_queue2.Count() > 0) return TopQueue2();
+
+        return 0;
+    }
+
+    // Pop
+    private int PopQueue1()
+    {
+        while (_queue1.Count() > 0)
+        {
+            var value = _queue1.Dequeue();
+
+            if (_queue1.Count() == 0)
+            {
+                return value;
+            }
+            else
+            {
+                _queue2.Enqueue(value);
+            }
+        }
+
+        return 0;
+    }
+    private int PopQueue2()
+    {
+        while (_queue2.Count() > 0)
+        {
+            var value = _queue2.Dequeue();
+
+            if (_queue2.Count() == 0)
+            {
+                return value;
+            }
+            else
+            {
+                _queue1.Enqueue(value);
+            }
+        }
+
+        return 0;
+    }
+
+    // Top
+    private int TopQueue1()
+    {
+        while (_queue1.Count() > 0)
+        {
+            var value = _queue1.Dequeue();
+
+            _queue2.Enqueue(value);
+
+            if (_queue1.Count() == 0)
+            {
+                return value;
+            }
+        }
+
+        return 0;
+    }
+    private int TopQueue2()
+    {
+        while (_queue2.Count() > 0)
+        {
+            var value = _queue2.Dequeue();
+
+            _queue1.Enqueue(value);
+
+            if (_queue2.Count() == 0)
+            {
+                return value;
+            }
+        }
+
+        return 0;
+    }
+
+    public bool Empty()
+    {
+        return _queue1.Count() == 0 && _queue2.Count() == 0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.Push(x);
+ * int param_2 = obj.Pop();
+ * int param_3 = obj.Top();
+ * bool param_4 = obj.Empty();
+ */
