a963527e6362d0ed90474668d7d9195fb7d2b275
Georgi Kamacharov <gkamacharov@gmail.com>
2022-07-25 12:23:52 -0400

    add sol

---
+++ LeetcodeProblemsCsharp/ExcelSheetColumnNumber/Solution.cs
@@ -0,0 +1,25 @@
+﻿// https://leetcode.com/problems/excel-sheet-column-number/
+public static class Solution
+{
+    private const string _cols = "abcdefghijklmnopqrstuvwxyz";
+    private const int _alphabetLetters = 26;
+
+    public static int TitleToNumber(string columnTitle)
+    {
+        var titleNumber = 0;
+        var columnTitleArray = columnTitle.ToCharArray();
+
+        for (var i = 0; i < columnTitleArray.Length - 1; i++)
+        {
+            titleNumber += (columnTitleArray[i] - 'A' + 1)
+                * (int)Math.Pow(_alphabetLetters, columnTitleArray.Length - 1 - i);
+        }
+
+        return titleNumber + GetPos(columnTitleArray[columnTitleArray.Length - 1]);
+    }
+
+    private static int GetPos(char col)
+    {
+        return _cols.IndexOf(col.ToString().ToLower()) + 1;
+    }
+}
