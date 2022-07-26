5b9243ff5fdf2202ef2911c8f15c4230efc2cddc
Georgi Kamacharov <gkamacharov@gmail.com>
2022-07-26 16:42:58 -0400

    add sol

---
+++ LeetcodeProblemsCsharp/PowerOfThree/Solution.cs
@@ -0,0 +1,16 @@
+﻿// https://leetcode.com/problems/power-of-three/
+public static class Solution
+{
+    public static bool IsPowerOfThree(int n)
+    {
+        for (var i = 0; i < n; i++)
+        {
+            var value = Math.Pow(3, i);
+
+            if (value == n) return true;
+            else if (value > n) return false;
+        }
+
+        return false;
+    }
+}
