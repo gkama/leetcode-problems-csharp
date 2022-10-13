05b5d34f040381e7337d4c17a4d55d1bee9759e0
Georgi Kamacharov <gkamacharov@gmail.com>
2022-10-13 11:05:48 -0400

    add sol

---
+++ LeetcodeProblemsCsharp/DailyByte.SwapWords/Solution.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Given a string s, reverse the words.
+ * Note: You may assume that there are no leading or trailing whitespaces and each word within s is only separated by a single whitespace.
+ * 
+ * Ex: Given the following string s…
+ * s = "The Daily Byte", return "Byte Daily The".
+ */
+public static class Solution
+{
+    public static string SwapWords(string s)
+    {
+        if (string.IsNullOrWhiteSpace(s)) return s;
+
+        var sSplit = s.Split(' ');
+        var sSplitSize = sSplit.Length;
+        var sReversed = new System.Text.StringBuilder();
+
+        for (var i = sSplitSize - 1; i >= 0; i--)
+        {
+            sReversed.Append(sSplit[i]);
+
+            if (i != 0) sReversed.Append(' ');
+        }
+
+        return sReversed.ToString();
+    }
+
+    public static void Examples()
+    {
+        var exs = new List<string>
+        {
+            "The Daily Byte",
+            "Testing The Reverse"
+        };
+
+        foreach (var ex in exs)
+        {
+            Console.WriteLine($"{ex} -> {SwapWords(ex)}");
+        }
+    }
+}
