bc53d72d75235caa1fa32964cd271050165da5c7
Georgi Kamacharov <gkamacharov@gmail.com>
2022-11-23 10:33:52 -0500

    add hft

---
+++ LeetcodeProblemsCsharp/DailyByte.HighFrequencyTrading/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyByte.HighFrequencyTrading
+{
+    /*
+     * You are given the list of prices of a particular stock. Each element in the array represents the price of the stock at a given second throughout the current day. Return the maximum profit you can make trading this stock.
+     * Note: You may only ever buy and sell a single share of the stock, but you may make multiple transactions (i.e. buys and sells).
+     * Ex: Given the following prices...
+     * 
+     * prices = [8, 3, 2, 4, 6, 4, 5], return 5.
+     */
+    public static class Solution
+    {
+        public static int Highest(int[] prices)
+        {
+            var highestProfit = 0;
+
+            return highestProfit;
+        }
+    }
+}
