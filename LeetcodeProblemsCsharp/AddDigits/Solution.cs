86258486f1eb0b80c93c65ae64ae01a9dc4b06bf
Georgi Kamacharov <gkamacharov@gmail.com>
2022-12-01 12:26:30 -0500

    add sol

---
+++ LeetcodeProblemsCsharp/AddDigits/Solution.cs
@@ -0,0 +1,24 @@
+﻿// https://leetcode.com/problems/add-digits/
+
+public static class Solution
+{
+    public static int AddDigits(int num)
+    {
+        return AddDigitsRecursive(num);
+    }
+
+    private static int AddDigitsRecursive(int num)
+    {
+        if (num < 10) return num;
+
+        var numStr = num.ToString();
+        var digitTotal = 0;
+
+        foreach (var digit in numStr)
+        {
+            digitTotal += int.Parse(digit.ToString());
+        }
+
+        return AddDigitsRecursive(digitTotal);
+    }
+}
