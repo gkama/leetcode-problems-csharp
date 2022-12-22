8baa8fd79735677810633019757b1bdfeb35b2fc
Georgi Kamacharov <gkamacharov@gmail.com>
2022-12-22 13:40:12 +0000

    add v2 sol

--- LeetcodeProblemsCsharp/DailyByte.StringValidity/Solution.cs
+++ LeetcodeProblemsCsharp/DailyByte.StringValidity/Solution.cs
@@ -8,9 +8,38 @@ public static class Solution
         if (string.IsNullOrWhiteSpace(s)) return false;
         if (s.Length <= 1) return false;
         if (s[0] == ')') return false;
+        if (s.Length % 2 == 1) return false;
 
         var stack = new Stack<char>();
         stack.Push(s[0]); // first one is either (
+
+        for (var i = 1; i < s.Length; i++)
+        {
+            if (s[i] == ')')
+            {
+                var p = stack.Pop();
+                if (p != '(') return false;
+            }
+            else
+            { 
+                stack.Push(s[i]);
+            }
+        }
+
+        if (stack.Count > 0) return false;
+
+        return true;
+    }
+
+    public static bool IsValidStringV2(string? s)
+    {
+        if (string.IsNullOrWhiteSpace(s)) return false;
+        if (s.Length <= 1) return false;
+        if (s[0] == ')') return false;
+        if (s.Length % 2 == 1) return false;
+
+        var stack = new Stack<char>();
+        stack.Push(s[0]); // first one is either ( or *
 
         for (var i = 1; i < s.Length; i++)
         {
